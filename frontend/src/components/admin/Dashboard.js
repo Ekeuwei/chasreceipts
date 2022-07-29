1eaeb3f3a7885b8e5e0f88a89ae71caa03dee3ac
Alfred Ekeuwei <>
2022-07-29 15:55:14 +0100

    Mobile friendly list

--- frontend/src/components/admin/Dashboard.js
+++ frontend/src/components/admin/Dashboard.js
@@ -35,13 +35,13 @@ const Dashboard = () => {
                     <Sidebar />
                 </div>
 
-                <div className="col-12 col-md-9">
+                <div className="col-12 col-md-9 px-4 px-md-3">
                     <h1 className="my-4">Dashboard</h1>
 
                     {loading ? <Loader /> : (
                         <Fragment>
                             <MetaData title={'Admin Dashboard'} />
-                            <div className="row pr-4">
+                            <div className="row pr-md-4">
                                 <div className="col-xl-12 col-sm-12 mb-3">
                                     <div className="card text-white bg-primary o-hidden h-100">
                                         <div className="card-body">
@@ -52,7 +52,7 @@ const Dashboard = () => {
                                 </div>
                             </div>
 
-                            <div className="row pr-4">
+                            <div className="row pr-md-4">
                                 <div className="col-xl-4 col-sm-6 mb-3">
                                     <div className="card text-white bg-success o-hidden h-100">
                                         <div className="card-body">
